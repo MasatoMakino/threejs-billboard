bf08c9db31a40a1950f6e9215b4ab5dbf4fdd721
MasatoMakino <unagiinu.g@gmail.com>
2019-05-05 16:55:39 +0900

    インスタンス生成後にスケールの変更ができるよう修正。

--- src/BillBoard.ts
+++ src/BillBoard.ts
@@ -8,14 +8,14 @@ import {
 
 export class BillBoard extends Sprite {
   url: string;
-  imageScale: number;
+  private _imageScale: number;
   texture: Texture;
 
   constructor(url: string, imageScale: number, option?: {}) {
     super();
 
     this.url = url;
-    this.imageScale = imageScale;
+    this._imageScale = imageScale;
 
     this.texture = new TextureLoader().load(this.url, this.onLoadTexture);
   }
@@ -27,11 +27,26 @@ export class BillBoard extends Sprite {
       depthTest: true,
       transparent: true
     });
+    this.updateScale();
+  };
+
+  private updateScale(): void {
+    if (this.texture == null || this.texture.image == null) return;
     const img = this.texture.image as HTMLImageElement;
+    console.log( this._imageScale);
     this.scale.set(
-      img.width * this.imageScale,
-      img.height * this.imageScale,
+      img.width * this._imageScale,
+      img.height * this._imageScale,
       1
     );
-  };
+  }
+
+  get imageScale(): number {
+    return this._imageScale;
+  }
+
+  set imageScale(value: number) {
+    this._imageScale = value;
+    this.updateScale();
+  }
 }
