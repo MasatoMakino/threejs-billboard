d71bf189a6c8a6f0ff1148abf4005c1334d298d6
MasatoMakino <unagiinu.g@gmail.com>
2019-05-23 14:06:06 +0900

    タスクランナー整理。

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,1 +1,24 @@
-exports.doc = require("./tsdoc")();
+"use strict";
+
+const { watch, src, dest, parallel, series } = require("gulp");
+
+const doc = require("gulptask-tsdoc")();
+exports.doc = doc;
+
+const server = require("gulptask-dev-server")("./docs/demo");
+exports.server = server;
+
+const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
+  "./webpack.config.js"
+);
+exports.bundleDevelopment = bundleDevelopment;
+
+const watchTasks = cb => {
+  watchBundle();
+  cb();
+};
+exports.watchTasks = watchTasks;
+
+exports.start_dev = series(watchTasks, server);
+
+exports.build = series(bundleDevelopment, doc);
--- task/browsersync.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const bs = require("browser-sync").create();
-
-const distDir = path.resolve(process.cwd(), "docs/demo");
-
-bs.init({
-  server: distDir,
-  watch: true
-});
