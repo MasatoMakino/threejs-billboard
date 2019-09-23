91e794ab76196e839c4e2ae75cb19421aeff3016
MasatoMakino <unagiinu.g@gmail.com>
2019-09-23 14:01:08 +0900

    bump to 0.2.5

--- bin/ScaleCalculator.d.ts
+++ bin/ScaleCalculator.d.ts
@@ -24,12 +24,25 @@ export declare class ScaleCalculator {
     private initRenderTarget;
     /**
      * カメラ側のプレーンの位置を更新する。
+     * このプレーンはカメラの位置と向きに一致する。
      */
-    private updatePlane;
+    updatePlane(): void;
     /**
      * targetがドットバイドット表示になるスケール値を算出する。
+     * プレーンから対象オブジェクトまでの距離を利用し、スケール値を逆算する。
+     *
+     * SpriteMaterial.sizeAttenuation = true[Default]
+     * の設定されたオブジェクト用。
+     * https://threejs.org/docs/#api/en/materials/SpriteMaterial.sizeAttenuation
+     *
      * @param target
      */
     getDotByDotScale(target: Object3D): number;
+    /**
+     * SpriteMaterial.sizeAttenuation = false
+     * の設定されたSprite用のスケール値を取得する。
+     */
+    getNonAttenuateScale(): number;
+    private getFovHeight;
 }
 //# sourceMappingURL=ScaleCalculator.d.ts.map
--- bin/ScaleCalculator.js
+++ bin/ScaleCalculator.js
@@ -40,22 +40,37 @@ export class ScaleCalculator {
     }
     /**
      * カメラ側のプレーンの位置を更新する。
+     * このプレーンはカメラの位置と向きに一致する。
      */
     updatePlane() {
         this.plane.setFromNormalAndCoplanarPoint(this._camera.getWorldDirection(this.worldDirection), this._camera.getWorldPosition(this.worldPosition));
     }
     /**
      * targetがドットバイドット表示になるスケール値を算出する。
+     * プレーンから対象オブジェクトまでの距離を利用し、スケール値を逆算する。
+     *
+     * SpriteMaterial.sizeAttenuation = true[Default]
+     * の設定されたオブジェクト用。
+     * https://threejs.org/docs/#api/en/materials/SpriteMaterial.sizeAttenuation
+     *
      * @param target
      */
     getDotByDotScale(target) {
         const size = this._renderer.getSize(new Vector2());
         const distance = this.plane.distanceToPoint(target.getWorldPosition(this.targetWorldPosition));
-        const getFovHeight = () => {
-            const halfFov = ThreeMath.degToRad(this._camera.fov / 2);
-            const half_fov_height = Math.tan(halfFov) * distance;
-            return half_fov_height * 2;
-        };
-        return getFovHeight() / size.height;
+        return this.getFovHeight(distance) / size.height;
+    }
+    /**
+     * SpriteMaterial.sizeAttenuation = false
+     * の設定されたSprite用のスケール値を取得する。
+     */
+    getNonAttenuateScale() {
+        const size = this._renderer.getSize(new Vector2());
+        return this.getFovHeight(1.0) / size.height;
+    }
+    getFovHeight(distance) {
+        const halfFov = ThreeMath.degToRad(this._camera.fov / 2);
+        const half_fov_height = Math.tan(halfFov) * distance;
+        return half_fov_height * 2;
     }
 }
