9a14d1c79c9330d9a73a14e980993e0344b93f42
MasatoMakino <unagiinu.g@gmail.com>
2019-12-05 16:01:41 +0900

    add : html copy task.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const { series } = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 
 const doc = require("gulptask-tsdoc").get();
 exports.doc = doc;
 
 const server = require("gulptask-dev-server").get("./docs/demo");
 exports.server = server;
+
+const copyGlob = "./demoSrc/**/*.{html,png,jpg,jpeg}";
+const copy = () => {
+  return src(copyGlob, { base: "./demoSrc/" }).pipe(dest("./docs/demo"));
+};
 
 const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
   "./webpack.config.js"
@@ -18,10 +23,11 @@ const { tsc, watchTsc } = require("gulptask-tsc").get();
 const watchTasks = cb => {
   watchBundle();
   watchTsc();
+  watch(copyGlob, copy);
   cb();
 };
 exports.watchTasks = watchTasks;
 
 exports.start_dev = series(watchTasks, server);
 
-exports.build = series(tsc, bundleDevelopment, doc);
+exports.build = series(tsc, copy, bundleDevelopment, doc);
