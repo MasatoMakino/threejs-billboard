25edb59cc5b66cfe17c1363f81e6ba25ed6443c4
MasatoMakino <unagiinu.g@gmail.com>
2021-10-17 13:20:02 +0900

    remove : createjs

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -9,7 +9,6 @@ const server = require("gulptask-dev-server").generateTask("./docs/demo");
 exports.server = server;
 
 const { bundleDemo, watchDemo } = require("gulptask-demo-page").generateTasks({
-  // externalScripts: ["https://code.createjs.com/1.0.0/createjs.min.js"],
   body: `<canvas id="webgl-canvas" width="640" height="480"></canvas>`,
 });
 
