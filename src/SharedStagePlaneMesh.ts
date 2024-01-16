571dbd509bcf2060e758b0cee64b990ca8522617
MasatoMakino <makino.masato.g@gmail.com>
2024-01-16 15:44:27 +0900

    refactor : Update UV attribute in SharedStageBillboard and SharedStagePlaneMesh

--- src/SharedStagePlaneMesh.ts
+++ src/SharedStagePlaneMesh.ts
@@ -1,6 +1,10 @@
 import { Material, Mesh, PlaneGeometry } from "three";
 import { CameraChaser } from "./CameraChaser.js";
-import { isSharedStageMaterial, TextureArea } from "./SharedStageTexture.js";
+import {
+  isSharedStageMaterial,
+  TextureArea,
+  updateUVAttribute,
+} from "./SharedStageTexture.js";
 
 /**
  * Canvasに描画可能な板オブジェクト。
@@ -61,15 +65,6 @@ export class SharedStagePlaneMesh extends Mesh {
    * ジオメトリにUV座標を設定する。
    */
   private updateUVAttribute(): void {
-    if (!isSharedStageMaterial(this.sharedMaterial)) {
-      throw new Error("sharedMaterial.map must be SharedStageTexture");
-    }
-    const area = this.sharedMaterial.map.calcurateUV(this._textureArea);
-    const uv = this.geometry.getAttribute("uv");
-    uv.setXY(0, area.x1, area.y2);
-    uv.setXY(1, area.x2, area.y2);
-    uv.setXY(2, area.x1, area.y1);
-    uv.setXY(3, area.x2, area.y1);
-    uv.needsUpdate = true;
+    updateUVAttribute(this.geometry, this.sharedMaterial, this._textureArea);
   }
 }
